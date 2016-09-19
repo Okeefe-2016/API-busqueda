0efc4e33317f220af7333c6cf7a872b1a2c85e49
Lucas Michailian <lucasmichailian@gmail.com>
2016-09-19 15:18:15 -0300

    emprendimientos

--- app/Repositories/UbicacionRepository.php
+++ app/Repositories/UbicacionRepository.php
@@ -16,10 +16,16 @@ class UbicacionRepository
     {
         $ubicationQuery = $this->getUbicationQuery();
 
+        if ($request->emp == ' is not null') {
+            $nomedif = 'AND  prop.nomedif = t4.nombre_ubicacion';
+        } else {
+            $nomedif = ' ';
+        }
+
         $query = "{$ubicationQuery}
                 LEFT JOIN propiedad AS prop 
                   ON t3.id_ubica = prop.id_ubica  
-                  AND prop.tipo_oper_id = $operacion AND prop.id_tipo_prop in($tipo)
+                  AND prop.tipo_oper_id = $operacion AND prop.id_tipo_prop in($tipo) " . $nomedif . " 
                 WHERE t0.nombre_ubicacion != t1.nombre_ubicacion
                   AND t4.nombre_ubicacion $request->emp
                 GROUP BY idZona
@@ -48,13 +54,10 @@ class UbicacionRepository
                     WHEN t2.id_ubica is null THEN t1.id_ubica
                     WHEN t3.id_ubica is null THEN t2.id_ubica
                     WHEN t4.id_ubica is null THEN t3.id_ubica
+                    WHEN  t3.nombre_ubicacion = t4.nombre_ubicacion THEN t3.id_ubica
                     ELSE t4.id_ubica
                    END) as idZona,
-                  (CASE
-                    WHEN t4.id_ubica is null THEN COUNT(prop.id_ubica)
-                    ELSE 
-                      (SELECT COUNT(id_emp) from emprendimiento where id_ubica = t4.id_ubica) 
-                   END) as cantidad,
+                  COUNT(prop.id_prop) as cantidad,
                   (CASE 
                     WHEN t2.nombre_ubicacion is null THEN CONCAT(t0.nombre_ubicacion,', ',t1.nombre_ubicacion)
                     WHEN t3.nombre_ubicacion is null THEN CONCAT(t0.nombre_ubicacion,', ',t1.nombre_ubicacion,',', t2.nombre_ubicacion)
@@ -65,7 +68,7 @@ class UbicacionRepository
                 LEFT JOIN ubicacionpropiedad AS t1 ON t1.id_padre = t0.id_ubica
                 LEFT JOIN ubicacionpropiedad AS t2 ON t2.id_padre = t1.id_ubica 
                 LEFT JOIN ubicacionpropiedad AS t3 ON t3.id_padre = t2.id_ubica 
-                LEFT JOIN ubicacionpropiedad AS t4 ON t4.id_padre = t3.id_ubica ";
+                LEFT JOIN ubicacionpropiedad AS t4 ON t4.id_padre = t3.id_ubica";
 
         return $ubicationQuery;
     }
